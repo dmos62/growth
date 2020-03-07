8291d58614fbd20355ed0ff7a8863381a99e2c7f
Steve Jain <mfiver@gmail.com>
2020-03-07 15:56:19 -0500

    Add monthly and daily periods
    Also require python3 since python2 appeared
    to be resulting in incorrect results.

--- dao-figures/README.md
+++ dao-figures/README.md
@@ -1,10 +1,12 @@
 # dao-figures
 
-This script parses all DAO transactions to produce quantitative reports of the Bisq DAO, per-cycle and all-time.
+This script parses all DAO transactions to produce quantitative reports of the Bisq DAO: per cycle, per month, per day, and all-time.
 
 ## Requirements
 
-Python 2
+Python 3 (tested on 3.6.9)
+
+_IMPORTANT: Python 2 will appear to work, but the resulting numbers will be INCORRECT._
 
 ## Instructions
 
@@ -13,4 +15,4 @@ Before you run the script, make sure to do the following:
 * Set the full path to your `btc_mainnet/db/json/tx` directory in `settings.env`
 * Start Bisq with `--dumpBlockchainData=true`
 
-Then run `python parse-txs.py`.
+Then run `python3 parse-txs.py`.
--- dao-figures/parse-txs.py
+++ dao-figures/parse-txs.py
@@ -2,8 +2,11 @@ import sys
 import os
 import json
 import copy
+import datetime
 
 cycleResults = {}
+monthlyResults = {}
+dailyResults = {}
 
 settingsFile = 'settings.env'
 pathError = 'Error finding DAO tx files...did you put the right path in ' + settingsFile + '? You must specify the full path (no tilde).'
@@ -91,6 +94,50 @@ def findCycle( height ):
 
     return cycle
 
+def getCalendarLabel( timestamp, needDay ):
+
+    calendarLabel = ''
+
+    month = datetime.datetime.fromtimestamp( timestamp ).month
+    year = datetime.datetime.fromtimestamp( timestamp ).year
+    day = datetime.datetime.fromtimestamp( timestamp ).day
+
+    calendarLabel = str(year)
+    calendarLabel += str(month) if month > 9 else '0' + str(month)
+
+    if( needDay == True ):
+        calendarLabel += str(day) if day > 9 else '0' + str(day)
+
+    return calendarLabel
+
+def writeJsonToCsv( timeType, destinationFile, dict ):
+
+    timeKeys = list( dict.keys() )
+    timeKeys.sort( reverse=True )
+    
+    with open( destinationFile, 'w' ) as f:
+
+        txLabels = ''
+        txKeys = list( dict[ timeKeys[0] ] )
+        for txKey in txKeys:
+            txLabels += ( txKey.lower() + '-count,' ) + ( txKey.lower() + '-fees,' )
+
+        if timeType == 'alltime':
+            pass
+        elif timeType == 'cycle':
+            f.write( 'cycle,' + txLabels[:-1] + '\n' )
+        elif timeType == 'monthly':
+            f.write( 'yearmonth,' + txLabels[:-1] + '\n' )
+        elif timeType == 'daily':
+            f.write( 'yearmonthday,' + txLabels[:-1] + '\n' )
+
+        for date in timeKeys:
+            csvData = ''
+            for txType in txKeys:
+                csvData += str( dict[ date ][ txType ][ 'count' ] ) + ',' + \
+                str( dict[ date ][ txType ][ 'feeSum' ] ) + ','
+            f.write( str(date) + ',' + csvData[:-1] + '\n' )
+
 ### set environment
 
 try:
@@ -102,37 +149,70 @@ except:
 
 ### iterate through dao transactions directory
 
-cycleResults = { 'ALL-TIME': copy.deepcopy( resultTemplate ) }    
+allTimeResults = copy.deepcopy( resultTemplate )
 
-try:
-    for filename in os.listdir( daoTxPath ):
-        
-        with open( daoTxPath + filename, 'r' ) as daoTx:
-            daoTxDict = json.loads( daoTx.read() )
+for filename in os.listdir( daoTxPath ):
+    
+    with open( daoTxPath + filename, 'r' ) as daoTx:
+        daoTxDict = json.loads( daoTx.read() )
+    
+    burntFee = daoTxDict[ 'burntFee' ] / 100
 
-        txCycle = findCycle( daoTxDict[ 'blockHeight' ] )
+    # cycle results
 
-        if( txCycle not in cycleResults ):
-            cycleResults[ txCycle ] = copy.deepcopy( resultTemplate )
-        
-        burntFee = daoTxDict[ 'burntFee' ] / 100
+    txCycle = findCycle( daoTxDict[ 'blockHeight' ] )
 
-        cycleResults[ txCycle ][ daoTxDict[ 'txType' ] ][ 'count' ] += 1
-        cycleResults[ txCycle ][ daoTxDict[ 'txType' ] ][ 'feeSum' ] += burntFee
+    if( txCycle not in cycleResults ):
+        cycleResults[ txCycle ] = copy.deepcopy( resultTemplate )
 
-        cycleResults[ txCycle ][ 'TOTAL' ][ 'count' ] += 1
-        cycleResults[ txCycle ][ 'TOTAL' ][ 'feeSum' ] += burntFee
+    cycleResults[ txCycle ][ daoTxDict[ 'txType' ] ][ 'count' ] += 1
+    cycleResults[ txCycle ][ daoTxDict[ 'txType' ] ][ 'feeSum' ] += burntFee
 
-        cycleResults[ 'ALL-TIME' ][ daoTxDict[ 'txType' ] ][ 'count' ] += 1
-        cycleResults[ 'ALL-TIME' ][ daoTxDict[ 'txType' ] ][ 'feeSum' ] += burntFee
+    cycleResults[ txCycle ][ 'TOTAL' ][ 'count' ] += 1
+    cycleResults[ txCycle ][ 'TOTAL' ][ 'feeSum' ] += burntFee
 
-        cycleResults[ 'ALL-TIME' ][ 'TOTAL' ][ 'count' ] += 1
-        cycleResults[ 'ALL-TIME' ][ 'TOTAL' ][ 'feeSum' ] += burntFee
+    # monthly results
 
-except IOError:
-    print( pathError )
-    sys.exit()
+    calendarLabel = getCalendarLabel( daoTxDict[ 'time' ] / 1000, False )
 
-### print results
+    if( calendarLabel not in monthlyResults ):
+        monthlyResults[ calendarLabel ] = copy.deepcopy( resultTemplate )
 
-print( json.dumps( cycleResults, indent=4 ) )
+    monthlyResults[ calendarLabel ][ daoTxDict[ 'txType' ] ][ 'count' ] += 1
+    monthlyResults[ calendarLabel ][ daoTxDict[ 'txType' ] ][ 'feeSum' ] += burntFee
+
+    monthlyResults[ calendarLabel ][ 'TOTAL' ][ 'count' ] += 1
+    monthlyResults[ calendarLabel ][ 'TOTAL' ][ 'feeSum' ] += burntFee
+
+    # daily results
+
+    calendarLabel = getCalendarLabel( daoTxDict[ 'time' ] / 1000, True )
+
+    if( calendarLabel not in dailyResults ):
+        dailyResults[ calendarLabel ] = copy.deepcopy( resultTemplate )
+
+    dailyResults[ calendarLabel ][ daoTxDict[ 'txType' ] ][ 'count' ] += 1
+    dailyResults[ calendarLabel ][ daoTxDict[ 'txType' ] ][ 'feeSum' ] += burntFee
+
+    dailyResults[ calendarLabel ][ 'TOTAL' ][ 'count' ] += 1
+    dailyResults[ calendarLabel ][ 'TOTAL' ][ 'feeSum' ] += burntFee
+
+    # all-time results
+
+    allTimeResults[ daoTxDict[ 'txType' ] ][ 'count' ] += 1
+    allTimeResults[ daoTxDict[ 'txType' ] ][ 'feeSum' ] += burntFee
+
+    allTimeResults[ 'TOTAL' ][ 'count' ] += 1
+    allTimeResults[ 'TOTAL' ][ 'feeSum' ] += burntFee
+
+### write results to file
+
+if not os.path.exists( "results" ):
+    os.makedirs( "results" )
+
+with open( 'results/dao-results-alltime.json', 'w' ) as f:
+    f.write( json.dumps( allTimeResults, indent=4 ) )
+
+writeJsonToCsv( 'cycle', 'results/dao-results-cycle.csv', cycleResults )
+writeJsonToCsv( 'monthly', 'results/dao-results-monthly.csv', monthlyResults )
+writeJsonToCsv( 'daily', 'results/dao-results-daily.csv', dailyResults )
